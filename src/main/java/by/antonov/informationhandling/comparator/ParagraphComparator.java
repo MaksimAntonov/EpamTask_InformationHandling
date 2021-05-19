e5091a0f06a7f84afa5402fe95ecbf960447cdac
Maxim Antonov <bol4onok@gmail.com>
2021-05-20 00:40:09 +0300

    feat: add comparator & test for comparator

---
+++ src/main/java/by/antonov/informationhandling/comparator/ParagraphComparator.java
@@ -0,0 +1,15 @@
+package by.antonov.informationhandling.comparator;
+
+import by.antonov.informationhandling.entity.ComponentType;
+import by.antonov.informationhandling.entity.TextComponent;
+import by.antonov.informationhandling.service.CalculationService;
+import java.util.Comparator;
+
+public class ParagraphComparator implements Comparator<TextComponent> {
+  @Override
+  public int compare(TextComponent paragraph1, TextComponent paragraph2) {
+    int sentences1 = CalculationService.countOfComponentsByType(paragraph1, ComponentType.SENTENCE);
+    int sentences2 = CalculationService.countOfComponentsByType(paragraph2, ComponentType.SENTENCE);
+    return sentences1 - sentences2;
+  }
+}
---
+++ src/test/java/by/antonov/informationhandling/comparator/ParagraphComparatorTest.java
@@ -0,0 +1,87 @@
+package by.antonov.informationhandling.comparator;
+
+import by.antonov.informationhandling.customparser.CustomParser;
+import by.antonov.informationhandling.customparser.ExpressionForSentence;
+import by.antonov.informationhandling.customparser.LexemeToTextElement;
+import by.antonov.informationhandling.customparser.ParagraphToSentenceParser;
+import by.antonov.informationhandling.customparser.SentenceToLexemeParser;
+import by.antonov.informationhandling.customparser.TextElementToCharacterParser;
+import by.antonov.informationhandling.customparser.TextToParagraphParser;
+import by.antonov.informationhandling.entity.BaseTextLeaf;
+import by.antonov.informationhandling.entity.ComponentType;
+import by.antonov.informationhandling.entity.TextComponent;
+import by.antonov.informationhandling.entity.TextComposite;
+import by.antonov.informationhandling.service.SearchService;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ParagraphComparatorTest {
+  private TextComponent rootComponent;
+  private TextComponent expectedComponent;
+
+  @BeforeClass
+  public void beforeClass() {
+    String string =
+        """
+              It has survived - not only (five) centuries, but also the leap into 13<<2 electronic typesetting, remaining 3>>5 essentially ~6&9|(3&4) unchanged. It was popularised in the 5|(1&2&(3|(4&(1^5|6&47)|3)|(~89&4|(42&7)))|1) with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using (~71&(2&3|(3|(2&1>>2|2)&2)|10&2))|78 Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using (Content here), content here', making it look like readable English.
+              It is a (7^5|1&2<<(2|5>>2&71))|1200 established fact that a reader will be of a page when looking at its layout.
+              Bye.
+            """;
+
+    this.rootComponent = getComponentForText(string);
+
+    String expectedString =
+        """
+              It is a (7^5|1&2<<(2|5>>2&71))|1200 established fact that a reader will be of a page when looking at its layout.
+              Bye.
+              It has survived - not only (five) centuries, but also the leap into 13<<2 electronic typesetting, remaining 3>>5 essentially ~6&9|(3&4) unchanged. It was popularised in the 5|(1&2&(3|(4&(1^5|6&47)|3)|(~89&4|(42&7)))|1) with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using (~71&(2&3|(3|(2&1>>2|2)&2)|10&2))|78 Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using (Content here), content here', making it look like readable English.
+            """;
+    this.expectedComponent = getComponentForText(expectedString);
+  }
+
+  @Test
+  public void compareTest() {
+    Comparator<TextComponent> componentComparator = new ParagraphComparator();
+    List<String> actual = rootComponent
+        .getComponentsByType(ComponentType.PARAGRAPH).orElse(new ArrayList<>())
+        .stream().sorted(componentComparator)
+        .map(Object::toString)
+        .collect(Collectors.toList());
+    List<String> expected = SearchService.getComponentsByType(expectedComponent, ComponentType.PARAGRAPH)
+        .stream().map(Objects::toString)
+        .collect(Collectors.toList());
+
+    Assert.assertEquals(actual, expected);
+  }
+
+  private TextComponent getComponentForText(String text) {
+    TextComponent component = new TextComposite(ComponentType.ROOT);
+    TextComponent baseText = new BaseTextLeaf(text);
+    component.add(baseText);
+
+    CustomParser textToParagraphParser = new TextToParagraphParser();
+    CustomParser sentenceParser = new ParagraphToSentenceParser();
+    CustomParser expressionParser = new ExpressionForSentence();
+    CustomParser lexemeParser = new SentenceToLexemeParser();
+    CustomParser textElementParser = new LexemeToTextElement();
+    CustomParser characterParser = new TextElementToCharacterParser();
+
+    textElementParser.nextParser(characterParser);
+    lexemeParser.nextParser(textElementParser);
+    expressionParser.nextParser(lexemeParser);
+    sentenceParser.nextParser(expressionParser);
+    textToParagraphParser.nextParser(sentenceParser);
+
+    textToParagraphParser.parse(component);
+
+    return component;
+  }
+}
